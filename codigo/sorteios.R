a7132f9a1ce6a3c26c57a99f1f4c42d947759c7a
marcelojsantos74 <marcelo.j.santos@unesp.br>
2023-10-26 22:18:42 -0300

    AJUSTE DO arquivo e exibicao

--- codigo/sorteios.R
+++ codigo/sorteios.R
@@ -1,7 +1,8 @@
 # Instalando pacotes
 if (!("ggplot2") %in% installed.packages()) install.packages("ggplot2")   
 if (!("dplyr") %in% installed.packages()) install.packages("dplyr")
-if (!("readr") %in% installed.packages()) install.packages("readr")
+if (!("readxl") %in% installed.packages()) install.packages("readr")
+if (!("writexl") %in% installed.packages()) install.packages("writexl")
 if (!("geobr") %in% installed.packages()) install.packages("geobr")
 if (!("scales") %in% installed.packages()) install.packages("scales")
 if (!("ggspatial") %in% installed.packages()) install.packages("ggspatial")
@@ -10,7 +11,8 @@ if (!("knitr") %in% installed.packages()) install.packages("knitr")
 
 library(ggplot2) # Visualizacao dos dados
 library(dplyr)   # Preparacao de dados
-library(readr)   # Importacao dos dados
+library(readxl)  # Importacao dos dados
+library(writexl) # Importacao dos dados
 library(geobr)   # Mapa do Brasil
 library(scales)
 library(ggspatial)
@@ -18,39 +20,36 @@ library(flexdashboard)
 library(knitr)
 library(kableExtra)
 
-
 # Diretorio de Trabalho
-setwd('D:/SINCID_SORTEIO')
+setwd('D:/sorteio-i-sincid-linda')
 
 # Dataframe Inscricoes Importacao arquivo CSV
-df.inscritos <-read.csv("./dados/INSCRITOS.csv", header = TRUE, sep = ";", quote = "\"", dec = ".")
+df.participantes <- read_excel("./dados/ListaCredenciamento.xlsx")
 
 # Dataframe Inscricoes Nao Sorteadas
-df.sorteio <- df.inscritos %>% filter(Sorteado == "N")
+df.sorteio <- df.participantes %>% filter(is.na(Sorteado))
 
 # Qtde de Inscricoes para Sorteio
 Qtde_Sorteio <- nrow(df.sorteio)
 
-# Numero Sorteado
-num_inscricao_sorteado <- sample(df.sorteio$Num_inscricao,1)
-num_inscricao_sorteado
+# ID Sorteado
+id_sorteado <- sample(df.sorteio$ID,1)
+id_sorteado
 
 # DataFrame Inscricao Sorteada
-df.sorteado <- df.inscritos[df.inscritos$Num_inscricao == num_inscricao_sorteado,]
-df.inscritos[df.inscritos$Num_inscricao == num_inscricao_sorteado,]$Sorteado = "S"
-df.inscritos[df.inscritos$Num_inscricao == num_inscricao_sorteado,]$DH_Sorteio = as.character(Sys.time())
+df.sorteado <- df.participantes[df.participantes$ID == id_sorteado,]
+
+df.participantes[df.participantes$ID == id_sorteado,]$Sorteado = "S"
+df.participantes[df.participantes$ID == id_sorteado,]$DH_Sorteio = as.character(Sys.time())
 
 # Dados do Sorteado
 Nome_sorteado <- df.sorteado$Nome
+Email_sorteado <-  df.sorteado$`E-mail`
 Cidade_sorteado <- df.sorteado$Cidade
-UF_sorteado <-  df.sorteado$UF
-Email_sorteado <-  df.sorteado$Email
-Telefone_sorteado <- df.sorteado$Telefone
-Modalidade_sorteado <- df.sorteado$Modalidade
+UF_sorteado <- df.sorteado$UF
 
 # Mostrar o sorteado
-sorteado <- data.frame(Nome_sorteado)
-
 
 # Gravar arquivo INSCRITO.CSV atualizado
-write_csv2(df.inscritos, file='./dados/INSCRITOS.csv')
+writexl::write_xlsx(df.participantes, './dados/ListaCredenciamento.xlsx')
+
